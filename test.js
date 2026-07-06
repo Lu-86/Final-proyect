6831a8a325f60a0ddb2e5ea93278fcbc86581e0c
lu-86 <gdluciano22@gmail.com>
2026-07-06 02:34:55 -0300

    bases de mi web

---
+++ test.js
@@ -0,0 +1,20 @@
+/* 
+function lista(a){
+    for (let i = 0; i < a.length; i++){
+        console.log(a[i]);
+    }
+    console.log("Aprobado");
+    return a;
+}
+
+let list = ["auto", "casa", "avion"];
+
+lista(list);
+*/
+
+function cambiarcolor(){
+    let botoncito = document.getElementById("boton");
+    botoncito.style.backgroundColor = "red";
+    botoncito.style.color = "white";
+}
+
